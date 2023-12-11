d094f8fc77e092e22f32f23a29802ce700d334c7
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-11 06:33:16 +0000

    view.pyupdateagain

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect, reverse
-from .models import CarMake, CarModel  # added this line
-# from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
-import json
-from django.http import JsonResponse  # added this line
+from .models import CarMake, CarModel, Dealer, DealerReview  # Import DealerReview model
+from django.http import HttpResponseNotAllowed
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -18,24 +15,20 @@ logger = logging.getLogger(__name__)
 
 # Create an `about` view to render a static about page
 def about(request):
-    context = {
-        "title": "about us"
-    }
+    context = {"title": "about us"}
     return render(request, "djangoapp/about.html", context)
 
 # Create a `contact` view to return a static contact page
 def contact(request):
-    context = {
-        "title": "contact us"
-    }
+    context = {"title": "contact us"}
     return render(request, "djangoapp/contact.html", context)
 
-# Create a `login_request` view to handle sign in request
+# Create a `login_request` view to handle sign-in request
 def login_request(request):
     context = {}
     if request.method == "GET":
         return render(request, "djangoapp/login.html")
-    
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("pwd")
@@ -46,15 +39,15 @@ def login_request(request):
             context["message"] = "ok"
         else:
             context["message"] = "Invalid details"
-        
+
         return render(request, "djangoapp/login.html", context)
 
-# Create a `logout_request` view to handle sign out request
+# Create a `logout_request` view to handle sign-out request
 def logout_request(request):
     logout(request)
     return redirect("djangoapp:login")
 
-# Create a `registration_request` view to handle sign up request
+# Create a `registration_request` view to handle sign-up request
 def registration_request(request):
     context = {}
     if request.method == "GET":
@@ -62,12 +55,12 @@ def registration_request(request):
 
     if request.method == "POST":
         context["message"] = ""
-        
-        email = request.POST.get('email') 
-        username = request.POST.get('username') 
-        first_name = request.POST.get('first_name') 
-        last_name = request.POST.get('last_name') 
-        pwd = request.POST.get('pwd') 
+
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        pwd = request.POST.get('pwd')
         c_pwd = request.POST.get('c-pwd')
 
         if pwd != c_pwd:
@@ -80,10 +73,10 @@ def registration_request(request):
                 message = "User already exists. Check email and/or username"
             except User.DoesNotExist:
                 user = User.objects.create(
-                    email = email,
-                    username = username,
-                    first_name = first_name,
-                    last_name = last_name,
+                    email=email,
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
                 )
                 user.set_password(pwd)
                 user.save()
@@ -94,8 +87,7 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
-    context["title"] = "Dealership Review"
+    context = {"title": "Dealership Review"}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
@@ -120,14 +112,46 @@ def dealer_reviews(request, dealer_id):
     data = {"dealer_reviews": list(reviews.values())}
     return JsonResponse(data)
 
-# Create a `update_dealer` view to update dealer details
+# Create an `update_dealer` view to update dealer details
 def update_dealer(request, dealer_id):
-    # This would be a POST request, you would get the new data from request.POST
-    # You would need to validate this data and then use it to update the dealer
-    pass
+    if request.method == "POST":
+        data = request.POST
+        # TODO: Validate the data
+
+        dealer = get_object_or_404(Dealer, id=dealer_id)
+
+        # Example: Update dealer name and location
+        dealer.name = data.get("name", dealer.name)
+        dealer.location = data.get("location", dealer.location)
+
+        # TODO: Continue updating other dealer fields as needed
+
+        dealer.save()
+
+        return JsonResponse({"message": "Dealer updated successfully"})
+    else:
+        return HttpResponseNotAllowed(["POST"])
 
 # Create a `post_review` view to post a dealer review
 def post_review(request, dealer_id):
-    # This would be a POST request, you would get the review data from request.POST
-    # You would need to validate this data and then use it to create a new DealerReview object
-    pass
+    if request.method == "POST":
+        data = request.POST
+        # TODO: Validate the data
+
+        dealer = get_object_or_404(Dealer, id=dealer_id)
+
+        # Create a new DealerReview object with the validated data
+        review = DealerReview.objects.create(
+            dealer=dealer,
+            title=data.get("title"),
+            content=data.get("content"),
+            rating=data.get("rating"),
+            # TODO: Add other fields as needed
+        )
+
+        # Save the new review to the database
+        review.save()
+
+        return JsonResponse({"message": "Review posted successfully"})
+    else:
+        return HttpResponseNotAllowed(["POST"])
