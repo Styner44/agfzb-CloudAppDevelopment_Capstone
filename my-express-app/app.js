ef5be0abc26d37afa23b113e26862e7c53cfaaba
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-11 04:19:28 +0000

    updatedapiperstate

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -1,20 +1,38 @@
-
 const express = require('express');
+const { CloudantV1, IamAuthenticator } = require('@ibm-cloud/cloudant');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Initialize Cloudant with settings from your IBM Cloud account
+const cloudant = CloudantV1.newInstance({ 
+  authenticator: new IamAuthenticator({ 
+    apikey: process.env.IAM_API_KEY 
+  }), 
+  serviceUrl: process.env.COUCH_URL 
+});
+
+// Use the 'dealerships' database
+const db = 'dealerships';
+
+app.get('/api/dealership', async (req, res) => {
+    try {
+        const state = req.query.state;
+        const response = await cloudant.postAllDocs({
+            db,
+            includeDocs: true
+        });
+        let dealerships = response.result.rows.map(row => row.doc);
+        if (state) {
+            dealerships = dealerships.filter(dealer => dealer.st === state);
+        }
+        res.json(dealerships);
+    } catch (err) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 });
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 });
-
-
-
-
-
-
-
-
