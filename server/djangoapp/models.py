995316a3b1b795e4435dbd8f1ffc51f07268caf1
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-11 08:31:52 +0000

    updated models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,30 +3,18 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Car Make model with fields: Name, Description, Country, Founded Date, etc.
 class CarMake(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(default=now)
-    # Additional fields
     country = models.CharField(max_length=255)
     founded_date = models.DateField()
 
     def __str__(self):
         return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# Car Model model with fields: Make, Name, Dealer ID, Type, Year, Engine, Price, MPG, etc.
 class CarModel(models.Model):
     SEDAN = 'SD'
     SUV = 'SV'
@@ -46,7 +34,6 @@ class CarModel(models.Model):
         default=SEDAN,
     )
     year = models.DateField()
-    # Additional fields
     engine = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     mpg = models.DecimalField(max_digits=5, decimal_places=2)
@@ -54,23 +41,26 @@ class CarModel(models.Model):
     def __str__(self):
         return self.name
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-class CarDealer:
-    def __init__(self, id, name, city, state, st):
-        self.id = id
-        self.name = name
-        self.city = city
-        self.state = state
-        self.st = st
+# Car Dealer model with fields: Name, City, State, ST
+class CarDealer(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    st = models.CharField(max_length=255)
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
-class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year):
-        self.dealership = dealership
-        self.name = name
-        self.purchase = purchase
-        self.review = review
-        self.purchase_date = purchase_date
-        self.car_make = car_make
-        self.car_model = car_model
-        self.car_year = car_year
+    def __str__(self):
+        return self.name
+
+# Dealer Review model with fields: Dealership, Name, Purchase, Review, Purchase Date, Car Make, Car Model, Car Year, etc.
+class DealerReview(models.Model):
+    dealership = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    purchase = models.BooleanField()
+    review = models.TextField()
+    purchase_date = models.DateField()
+    car_make = models.CharField(max_length=255)
+    car_model = models.CharField(max_length=255)
+    car_year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
