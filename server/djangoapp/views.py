6c3075598a039ed96ea066aef434ce891a7c8c64
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-12 04:22:40 +0000

    views.py: Contains several view functions, each of which handles a specific type of HTTP request to a specific URL.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,39 +1,45 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse, HttpResponseNotAllowed
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 import logging
-from .models import CarMake, CarModel, CarDealer, DealerReview
-from django.shortcuts import redirect
+from .models import CarModel, Dealer, DealerReview
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import FunctionsV1
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-# Create your views here.
+# Set up IBM Cloud Functions client
+authenticator = IAMAuthenticator('your-ibm-cloud-api-key')
+functions = FunctionsV1(authenticator=authenticator)
 
-# Create an `about` view to render a static about page
+# Views for static pages
 def about(request):
+    # Render the about page
     context = {"title": "about us"}
     return render(request, "djangoapp/about.html", context)
 
-# Create a `contact` view to return a static contact page
 def contact(request):
+    # Render the contact page
     context = {"title": "contact us"}
     return render(request, "djangoapp/contact.html", context)
 
-# Create a `login_request` view to handle sign-in request
+# View for handling user login
 def login_request(request):
     context = {}
     if request.method == "GET":
         return render(request, "djangoapp/login.html")
 
     if request.method == "POST":
+        # Attempt to authenticate the user
         username = request.POST.get("username")
         password = request.POST.get("pwd")
 
         user = authenticate(username=username, password=password)
         if user:
+            # Log in the user if authentication is successful
             login(request, user)
             context["message"] = "ok"
         else:
@@ -41,12 +47,13 @@ def login_request(request):
 
         return render(request, "djangoapp/login.html", context)
 
-# Create a `logout_request` view to handle sign-out request
+# View for handling user logout
 def logout_request(request):
+    # Log out the user and redirect to the login page
     logout(request)
     return redirect("djangoapp:login")
 
-# Create a `registration_request` view to handle sign-up request
+# View for handling user registration
 def registration_request(request):
     context = {}
     if request.method == "GET":
@@ -63,14 +70,18 @@ def registration_request(request):
         c_pwd = request.POST.get('c-pwd')
 
         if pwd != c_pwd:
+            # Passwords do not match
             message = "Passwords do not match"
-        elif User.objects.filter(email=email):
+        elif User.objects.filter(email=email).exists():
+            # Email is already in use
             message = "Email already in use"
         else:
             try:
+                # Check if the username already exists
                 User.objects.get(username=username)
                 message = "User already exists. Check email and/or username"
             except User.DoesNotExist:
+                # Create a new user
                 user = User.objects.create(
                     email=email,
                     username=username,
@@ -84,39 +95,51 @@ def registration_request(request):
         context["message"] = message
         return render(request, "djangoapp/registration.html", context)
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
+# View for rendering the index page with a list of dealerships
 def get_dealerships(request):
     context = {"title": "Dealership Review"}
     if request.method == "GET":
+        try:
+            # Call the 'dealer-get' service
+            response = functions.invoke_action('your-namespace', 'dealer-get').get_result()
+
+            # Extract the dealerships from the response
+            dealerships = response['dealerships']
+
+            # Add the dealerships to the context
+            context['dealerships'] = dealerships
+
+        except Exception as e:
+            # Handle any errors that might occur during the API call
+            context['error'] = f"Error retrieving dealerships: {str(e)}"
+
+        # Render the 'index.html' template with the updated context
         return render(request, 'djangoapp/index.html', context)
 
-# Create a `car_models` view to get a list of all car models
+# View for getting a list of all car models
 def car_models(request):
     models = CarModel.objects.all()
     data = {"car_models": list(models.values("name", "description"))}
     return JsonResponse(data)
 
-# Create a `dealer_reviews` view to get reviews of a dealer
+# View for getting reviews of a dealer
 def dealer_reviews(request, dealer_id):
-    # Assuming you have a DealerReview model with a foreign key to the Dealer model
     reviews = DealerReview.objects.filter(dealer_id=dealer_id)
     data = {"dealer_reviews": list(reviews.values("name", "review", "purchase_date", "purchase", "car_make", "car_model", "car_year", "sentiment"))}
     return JsonResponse(data)
 
-# Create an `update_dealer` view to update dealer details
+# View for updating dealer details
 def update_dealer(request, dealer_id):
     if request.method == 'POST':
         # Check if the user is authenticated and is staff
-        if not request.user.is_authenticated or not request.user.is_staff:
+        if not (request.user.is_authenticated and request.user.is_staff):
             return JsonResponse({"error": "Unauthorized"}, status=403)
 
         # Validate POST data
         required_fields = ['name', 'city', 'state', 'st']
         for field in required_fields:
-            if field not in request.POST:
-                return JsonResponse({'error': f'Missing required field: {field}'}, status=400)
-            if not isinstance(request.POST[field], str):
-                return JsonResponse({'error': f'Invalid type for field: {field}. Expected string.'}, status=400)
+            if field not in request.POST or not isinstance(request.POST[field], str):
+                return JsonResponse({'error': f'Missing or invalid field: {field}'}, status=400)
 
         try:
             # Update dealer details
@@ -133,13 +156,13 @@ def update_dealer(request, dealer_id):
         except Dealer.DoesNotExist:
             return JsonResponse({'error': 'Dealer does not exist'}, status=404)
         except Exception as e:
-                      # An unexpected error occurred
+            # An unexpected error occurred
             logger.error(f"Error updating dealer: {str(e)}")
             return JsonResponse({'error': 'An unexpected error occurred'}, status=500)
     else:
         return HttpResponseNotAllowed(["POST"])
 
-# Create a `post_review` view to post a dealer review
+# View for posting a dealer review
 def post_review(request, dealer_id):
     if request.method == "POST":
         if not request.user.is_authenticated:
