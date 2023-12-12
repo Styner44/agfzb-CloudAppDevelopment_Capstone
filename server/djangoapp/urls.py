a6b2c0ba95d0cb9e98309fdb08e7f138e6b079df
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-12 06:34:12 +0000

    urls.py: Code includes the two paths for dealer reviews and posting reviews that you specified.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,12 +5,10 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
+    # path for about page
     path('about/', views.about, name="about"),
 
+    # path for contact page
     path('contact/', views.contact, name="contact"),
 
     # path for registration
@@ -22,10 +20,13 @@ urlpatterns = [
     # path for logout
     path('logout/', views.logout_request, name="logout"),
 
+    # default path for index (homepage)
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path('dealer_reviews/<int:dealer_id>/', views.dealer_reviews, name="dealer_reviews"),
 
     # path for add a review view
-
+    path('post_review/<int:dealer_id>/', views.post_review, name="post_review"),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
