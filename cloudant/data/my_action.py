fbed0a9a29b4b7cdc826471e9488661794f461d7
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-12 04:01:20 +0000

    my_action.py: Connected to IBM Cloud Functions using the IBM Cloud Functions API from your Python code.

--- cloudant/data/my_action.py
+++ cloudant/data/my_action.py
@@ -1,6 +1,8 @@
 import json
 from urllib.parse import urlparse
-from cloudant.client import Cloudant  # This is the import statement
+from cloudant.client import Cloudant
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import FunctionsV1
 
 def main(dict):
     url = urlparse(dict["COUCH_URL"])
@@ -19,3 +21,13 @@ with open('../../functions/.creds.json', 'r') as f:
 
 # Call the main function with the credentials
 print(main(credentials))
+
+# Use the IBM Cloud API key from the credentials
+authenticator = IAMAuthenticator(credentials["IAM_API_KEY"])
+functions = FunctionsV1(authenticator=authenticator)
+
+response = functions.list_actions().get_result()
+
+actions = response['actions']
+for action in actions:
+    print(action['name'])
