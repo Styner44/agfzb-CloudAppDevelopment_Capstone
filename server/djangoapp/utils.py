186293981d4853a0d41ae64ca8bb503d1c23b980
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-19 00:54:33 +0000

    utils.py added code

---
+++ server/djangoapp/utils.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_dealer_reviews_from_cf(dealer_id):
+    # Implementation for retrieving dealer reviews from CF
+    pass
+
+def get_dealers_from_cf():
+    # Implementation for retrieving dealers from CF
+    pass
