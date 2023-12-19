ee70e2db6d7a6c4a13044f2a4b6e8486eb6964ef
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-19 02:00:58 +0000

    updated restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,17 +1,19 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from djangoapp.models import CarDealer, DealerReview
+from djangoapp.models import DealerReview
 
 # Define the missing variable
 api_key = "your_api_key"
 
-# Define the missing get_request function
+# Define the get_request function
 def get_request(url, params=None, headers=None, auth=None):
-    return requests.get(url, params=params, headers=headers, auth=auth)
+    response = requests.get(url, params=params, headers=headers, auth=auth)
+    return response.json()
 
-# Define the missing post_request function
+# Define the post_request function
 def post_request(url, payload=None, headers=None):
-    return requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
 
 # Create a `get_request` to make HTTP GET requests
 def get_dealer_reviews_from_cf(url, dealerId):
@@ -38,5 +40,5 @@ def analyze_review_sentiments(text):
     # Call post_request() to analyze text
     response = post_request('https://watson-nlu-api', payload={'text': text}, headers={'Content-Type': 'application/json'})
     # Get the returned sentiment label such as Positive or Negative
-    sentiment_label = response.get('sentiment', 'Unknown')
+    sentiment_label = response.get('sentiment', {}).get('label', 'Unknown')
     return sentiment_label
