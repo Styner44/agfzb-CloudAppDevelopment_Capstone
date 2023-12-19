34a6f54bf79ff927b421a163f417a8226be2fe69
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-19 02:55:58 +0000

    restapis.py updated

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,32 +11,39 @@ def get_request(url, params=None, headers=None, auth=None):
         response = requests.get(url, params=params, headers=headers, auth=auth)
     else:
         response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()  # Raise HTTPError for bad responses
     return response.json()
-
-def analyze_review_sentiments(url, **kwargs):
-    params = dict()
-    params["text"] = kwargs["text"]
-    params["version"] = kwargs["version"]
-    params["features"] = kwargs["features"]
-    params["return_analyzed_text"] = kwargs["return_analyzed_text"]
-    response = get_request(url, params=params, headers={'Content-Type': 'application/json'},
-                           auth=HTTPBasicAuth('apikey', api_key) if api_key else None)
-    return response
 
 # Define the post_request function
 def post_request(url, payload=None, headers=None):
     response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()  # Raise HTTPError for bad responses
     return response.json()
 
-# Create a `get_request` to make HTTP GET requests
+# Define the get_dealer_reviews_from_cf function
 def get_dealer_reviews_from_cf(url, dealerId):
     try:
-        data = get_request(url, params={'dealerId': dealerId}, headers={'Content-Type': 'application/json'},
-                          auth=HTTPBasicAuth('username', 'password'))  # Replace 'username' and 'password' with your actual username and password
+        data = get_request(
+            url,
+            params={'dealerId': dealerId},
+            headers={'Content-Type': 'application/json'},
+            auth=HTTPBasicAuth('username', 'password'),  # Replace with actual username and password
+        )
         reviews = []
         if 'docs' in data:
             for doc in data['docs']:
-                review = DealerReview(dealership=doc['dealership'], name=doc['name'], purchase=doc['purchase'], review=doc['review'], purchase_date=doc['purchase_date'], car_make=doc['car_make'], car_model=doc['car_model'], car_year=doc['car_year'], sentiment=doc['sentiment'], id=doc['id'])
+                review = DealerReview(
+                    dealership=doc['dealership'],
+                    name=doc['name'],
+                    purchase=doc['purchase'],
+                    review=doc['review'],
+                    purchase_date=doc['purchase_date'],
+                    car_make=doc['car_make'],
+                    car_model=doc['car_model'],
+                    car_year=doc['car_year'],
+                    sentiment=doc['sentiment'],
+                    id=doc['id'],
+                )
                 reviews.append(review)
         return reviews
     except requests.exceptions.HTTPError as errh:
@@ -49,16 +56,30 @@ def get_dealer_reviews_from_cf(url, dealerId):
         print("Error:", err)
 
 # Define the get_data_from_url function
-def get_data_from_url(url, api_key):
+def get_data_from_url(url):
     params = {}  # Add any parameters you need for the request here
-    response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-                            auth=HTTPBasicAuth('username', 'password'))  # Replace 'username' and 'password' with your actual username and password
+    response = requests.get(
+        url,
+        params=params,
+        headers={'Content-Type': 'application/json'},
+        auth=HTTPBasicAuth('username', 'password'),  # Replace with actual username and password
+    )
+    response.raise_for_status()  # Raise HTTPError for bad responses
     return response.json()  # Returns the response as a JSON object
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 def analyze_review_sentiments(text):
     # Call post_request() to analyze text
-    response = post_request('https://watson-nlu-api', payload={'text': text}, headers={'Content-Type': 'application/json'})
+    response = post_request(
+        'https://watson-nlu-api',
+        payload={'text': text},
+        headers={'Content-Type': 'application/json'},
+    )
     # Get the returned sentiment label such as Positive or Negative
     sentiment_label = response.get('sentiment', {}).get('label', 'Unknown')
     return sentiment_label
+
+if __name__ == "__main__":
+    text = "This is a review text."
+    sentiment_label = analyze_review_sentiments(text)
+    print(sentiment_label)
