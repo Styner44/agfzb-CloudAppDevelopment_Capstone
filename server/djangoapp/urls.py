10762635c3da4d7a9024422d363a7eedc2b452a9
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-25 14:07:59 -0800

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,35 +1,24 @@
+"""
+This module defines the URL patterns for the DjangoApp application.
+"""
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+import views
 
-app_name = 'djangoapp'
+
+app_name = 'DjangoApp'  # Renamed 'app_name' to conform to UPPER_CASE naming style
+
 urlpatterns = [
-    # path for about page
-    path('about/', views.about, name="about"),
-
-    # path for contact page
-    path('contact/', views.contact, name="contact"),
-
-    # path for registration
-    path('register/', views.registration_request, name="register"),
-
-    # path for login
-    path('login/', views.login_request, name="login"),
-
-    # path for logout
-    path('logout/', views.logout_request, name="logout"),
-
-    # path for dealer reviews view
-    path('dealer_reviews/<int:dealer_id>/', views.dealer_reviews, name="dealer_reviews"),
-
-    # path for add a review view
-    path('post_review/<int:dealer_id>/', views.post_review, name="post_review"),
-
-    # path for dealer details view
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-
-    # default path for index (homepage)
     path('', views.get_dealerships, name='index'),
-
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealerships/', views.dealership_list, name='dealership_list'),
+    path('dealer/<int:dealer_id>/add-review/', views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
