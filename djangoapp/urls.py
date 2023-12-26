deac5c7c3a93b6728ea56e05b7bce93d7a289874
Styner <145401330+Styner2023@users.noreply.github.com>
2023-12-25 17:08:49 -0800

    Update urls.py

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -4,20 +4,21 @@ This module defines the URL patterns for the DjangoApp application.
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-import views
+from djangoapp import views
 
 
 app_name = 'DjangoApp'  # Renamed 'app_name' to conform to UPPER_CASE naming style
 
 urlpatterns = [
-    path('', views.get_dealerships, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('registration/', views.registration_request, name='registration'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-    path('dealerships/', views.dealership_list, name='dealership_list'),
-    path('dealer/<int:dealer_id>/add-review/', views.add_review, name="add_review"),
+    path("kishana",views.get_dealerships,name='TEST')
+    # path('', views.get_dealerships, name='index'),
+    # path('about/', views.about, name='about'),
+    # path('contact/', views.contact, name='contact'),
+    # path('registration/', views.registration_request, name='registration'),
+    # path('login/', views.login_request, name='login'),
+    # path('logout/', views.logout_request, name='logout'),
+    # path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    # path('dealerships/', views.dealership_list, name='dealership_list'),
+    # path('dealer/<int:dealer_id>/add-review/', views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
